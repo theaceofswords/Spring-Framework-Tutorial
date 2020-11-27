8154193da0c1ca8dde5f9643e551c6ddff89f430
navaneeth.k <navaneeth.k@qburst.com>
2020-11-27 12:40:36 +0530

    Added html pages and controllers

---
+++ petclinic/src/main/java/com/springexample/petclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.springexample.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.html"}) //when a request comes in for a root or for index it will
+    private String index(){                         //match to  this
+        return "index";
+    }
+}
---
+++ petclinic/src/main/java/com/springexample/petclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package com.springexample.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets","/vets/index","vets/index.html"})
+    public String listVets(){
+        return "Vets/index";
+    }
+}
