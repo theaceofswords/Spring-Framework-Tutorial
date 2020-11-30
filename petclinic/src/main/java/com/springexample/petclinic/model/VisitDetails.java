87f75dc6c541883bdf0770644a79b4b41b2877cf
navaneeth.k <navaneeth.k@qburst.com>
2020-11-30 21:00:40 +0530

    Added styles and new entities

---
+++ petclinic/src/main/java/com/springexample/petclinic/model/VisitDetails.java
@@ -0,0 +1,33 @@
+package com.springexample.petclinic.model;
+
+import java.time.LocalDate;
+
+public class VisitDetails extends BaseEntity {
+    private LocalDate localDate;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
